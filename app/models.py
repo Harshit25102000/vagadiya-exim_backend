f2bb22ac714df9ed030240f2fe40b72dea9cc429
Yogesh Sirsat <yogeshsirsat56@gmail.com>
2024-07-12 00:02:29 +0530

    Delete old media files on update or delete of model.

--- app/models.py
+++ app/models.py
@@ -15,6 +15,26 @@ class HomePage(models.Model):
         verbose_name = "Home Page Detail"
         verbose_name_plural = "Home Page Detail"
 
+    # delete old image on update
+    def save(self, *args, **kwargs):
+        try:
+            this = HomePage.objects.get(id=self.id)
+            if (
+                this.top_banner_image != self.top_banner_image
+                and this.top_banner_image
+            ):
+                this.top_banner_image.delete(save=False)
+        except HomePage.DoesNotExist:
+            pass
+        super(HomePage, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.top_banner_image.delete(save=False)
+        super(HomePage, self).delete(*args, **kwargs)
+
+    def __str__(self):
+        return self.top_banner_title
+
 
 class HomePageBottomBanner(models.Model):
     bottom_banner_title = models.CharField(max_length=100)
@@ -39,6 +59,20 @@ class HomePageServicesItem(models.Model):
     class Meta:
         verbose_name = "Home Page Services Item"
         verbose_name_plural = "Home Page Services Items"
+
+    # delete old image on update
+    def save(self, *args, **kwargs):
+        try:
+            this = HomePageServicesItem.objects.get(id=self.id)
+            if this.service_image != self.service_image and this.service_image:
+                this.service_image.delete(save=False)
+        except HomePageServicesItem.DoesNotExist:
+            pass
+        super(HomePageServicesItem, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.service_image.delete(save=False)
+        super(HomePageServicesItem, self).delete(*args, **kwargs)
 
 
 class ProvidingsItemPoint(models.Model):
@@ -67,6 +101,23 @@ class HomePageProvidingsItem(models.Model):
         verbose_name = "Home Page Providings Item"
         verbose_name_plural = "Home Page Providings Items"
 
+    # delete old image on update
+    def save(self, *args, **kwargs):
+        try:
+            this = HomePageProvidingsItem.objects.get(id=self.id)
+            if (
+                this.providing_image != self.providing_image
+                and this.providing_image
+            ):
+                this.providing_image.delete(save=False)
+        except HomePageProvidingsItem.DoesNotExist:
+            pass
+        super(HomePageProvidingsItem, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.providing_image.delete(save=False)
+        super(HomePageProvidingsItem, self).delete(*args, **kwargs)
+
 
 class AboutUsPage(models.Model):
     title = models.CharField(max_length=100)
@@ -84,6 +135,20 @@ class AboutUsPage(models.Model):
         verbose_name = "About Us Page"
         verbose_name_plural = "About Us Page"
 
+    # delete old video on update
+    def save(self, *args, **kwargs):
+        try:
+            this = AboutUsPage.objects.get(id=self.id)
+            if this.video != self.video and this.video:
+                this.video.delete(save=False)
+        except AboutUsPage.DoesNotExist:
+            pass
+        super(AboutUsPage, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.video.delete(save=False)
+        super(AboutUsPage, self).delete(*args, **kwargs)
+
 
 class AboutUsStory(models.Model):
     title = models.CharField(max_length=100)
@@ -94,6 +159,20 @@ class AboutUsStory(models.Model):
     class Meta:
         verbose_name = "About Us Story"
         verbose_name_plural = "About Us Story"
+
+    # delete old image on update
+    def save(self, *args, **kwargs):
+        try:
+            this = AboutUsStory.objects.get(id=self.id)
+            if this.image != self.image and this.image:
+                this.image.delete(save=False)
+        except AboutUsStory.DoesNotExist:
+            pass
+        super(AboutUsStory, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.image.delete(save=False)
+        super(AboutUsStory, self).delete(*args, **kwargs)
 
 
 class AboutUsStoryValue(models.Model):
@@ -197,3 +276,29 @@ class Product(models.Model):
     class Meta:
         verbose_name = "Product"
         verbose_name_plural = "Products"
+
+    # delete old images on update
+    def save(self, *args, **kwargs):
+        try:
+            this = Product.objects.get(id=self.id)
+            if this.image_1 != self.image_1 and this.image_1:
+                this.image_1.delete(save=False)
+            if this.image_2 != self.image_2 and this.image_2:
+                this.image_2.delete(save=False)
+            if this.image_3 != self.image_3 and this.image_3:
+                this.image_3.delete(save=False)
+            if this.image_4 != self.image_4 and this.image_4:
+                this.image_4.delete(save=False)
+            if this.image_5 != self.image_5 and this.image_5:
+                this.image_5.delete(save=False)
+        except Product.DoesNotExist:
+            pass
+        super(Product, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.image_1.delete(save=False)
+        self.image_2.delete(save=False)
+        self.image_3.delete(save=False)
+        self.image_4.delete(save=False)
+        self.image_5.delete(save=False)
+        super(Product, self).delete(*args, **kwargs)
